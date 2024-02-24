55c9e227985b4f603b8c191373e6621f47019499
jessie-moe <jessie.moe@email.phoenix.edu>
2024-02-24 07:18:16 -0800

    Python program to take input of first and last name and display on screen

---
+++ input_name.py
@@ -0,0 +1,8 @@
+# Simple imput statement to take and display first and last name
+# By Jessie Moe
+
+fname = input('May I have your first name please? ')    # Input and assign first name
+lname = input('May I have your last name please? ')     # Input and assign last name
+print('Thank You')
+
+print('\nYounr name is ' + fname + ' ' + lname + '.')   # Display first and last name together
