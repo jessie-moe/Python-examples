64dc1d77e1240156369efcd349bbf684211d4654
jessie-moe <jessie.moe@email.phoenix.edu>
2024-02-24 08:08:21 -0800

    basic add, sub, mult, divide. It does not catch 0 division error currently. I will fix in future update

---
+++ two_number_basic_calculator.py
@@ -0,0 +1,9 @@
+a = float(input('Enter first number: '))# input a float value for variable a here
+b = float(input('Enter second number: '))# input a float value for variable b here
+
+print(f"{a} + {b} = ", str(a+b))# output the result of addition here
+print(f"{a} - {b} = ", str(a-b))# output the result of subtraction here
+print(f"{a} * {b} = ", str(a*b))# output the result of multiplication here
+print(f"{a} / {b} = ", str(b/a))# output the result of division here, does not protect against 0 division error
+
+print("Thanks")
